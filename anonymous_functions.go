23f7b8effe8de4206b077ed8fb3855bbfb67d23f
tejas-2232 <tejasbachhav98@gmail.com>
2025-03-17 19:33:51 -0400

    passing anonymous func as arguments

--- anonymous_functions.go
+++ anonymous_functions.go
@@ -3,6 +3,9 @@ An anonymous function is a function that doesn’t have a name.
 It is useful when you want to create an inline function.
 In Go, an anonymous function can also form a closure. An anonymous function is also known as a function literal
 */
+//*********************************************************
+// RUN EACH BLOCK AS SEPARATE CODE BY UNCOMMENTING IT   -  T H A N K S
+//*********************************************************
 
 // package main
 // import "fmt"
@@ -30,12 +33,33 @@ You can assign an anonymous function to a variable. This variable can then be ca
 /*Passing Arguments
 Anonymous functions can accept arguments.*/
 
+// package main
+// import "fmt"
+// func main() {
+// 	func(ele string) {
+// 		fmt.Println(ele)
+// 	}("French revolution")
+// }
+
+/*Passing as Arguments
+You can also pas an anonymous func as argument to another function
+*/
+
 package main
 
 import "fmt"
 
+func galaxy(i func(p, q string) string) {
+	fmt.Println(i("Golden ", "Hour "))
+}
+
+// Think of i as a placeholder for a function.
+// Just like we pass integers or strings as arguments, we are passing a function here.
+
 func main() {
-	func(ele string) {
-		fmt.Println(ele)
-	}("French revolution")
+
+	value := func(p, q string) string {
+		return p + q + "at beach"
+	}
+	galaxy(value) // The galaxy function is called with value as its argument
 }
