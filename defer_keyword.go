7dc68c23cb38a7cb6a917d6690ebe5190120a29c
tejas-2232 <tejasbachhav98@gmail.com>
2025-03-22 23:58:28 -0400

    what is defer keyword in go

---
+++ defer_keyword.go
@@ -0,0 +1,38 @@
+/*
+In Go language, defer statements delay the execution of the function or method or an anonymous method until the
+nearby functions returns. In other words, defer function or method call arguments evaluate instantly,
+but they don’t execute until the nearby functions returns. You can create a deferred method, or function, or
+ anonymous function by using the defer keyword.
+*/
+// concept of the defer statement
+
+package main
+
+import "fmt"
+
+func mul(a1, a2 int) int {
+
+	res := a1 * a2
+	fmt.Println("Result= ", res)
+	return 0
+}
+
+func show() {
+	fmt.Println("Hello there")
+}
+
+func main() {
+	// Calling mul() function
+	// Here mul function behaves
+	// like a normal function
+	mul(20, 5)
+
+	// Calling mul()function Using defer keyword
+	// Here the mul() function is defer function
+
+	defer mul(30, 4)
+
+	// Calling show() function
+	show()
+
+}
