f832cec0ceb70bf023e92e61f66ce86ee1b46a63
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-14 00:05:51 -0400

    how to copy array

--- arrays.go
+++ arrays.go
@@ -2,12 +2,28 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("Array:")
+
+// 	var arr1 [3]int = [3]int{1, 2, 3}
+
+// 	fmt.Println("Array=", arr1)
+// }
+
+//copying array into another
+// both array should have same length and type
+
+var source = [5]int{10, 20, 30, 40, 50}
+
 func main() {
-	fmt.Println("Array:")
 
-	var arr1 [3]int = [3]int{1, 2, 3}
+	var destination [5]int
 
-	fmt.Println("Array=", arr1)
+	for i := 0; i < len(source); i++ {
+		destination[i] = source[i]
+	}
+
+	fmt.Println("source array:", source)
+	fmt.Println("destination array:", destination)
+
 }
-
-//copying array
