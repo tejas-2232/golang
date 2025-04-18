f4ca3d737063e757dd18c35ac8c533dc0ec18dfd
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-17 22:08:28 -0400

    file update

--- project/students_api/cmd/students_api/main.go
+++ project/students_api/cmd/students_api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tejas-2232/students_api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students api")
+	// load config
+	cfg := config.MustLoad()
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		// return data
+		w.Write([]byte("Welcome to students API")) //converting string to byte slice & passing to write func
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("server started %s", cfg.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+
 }
