49c8d4a5e454ba6bde3c67b902a2129b99c0b34d
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-19 17:45:40 -0400

    go channel creation and logging for graceful shutdown

--- project/students_api/cmd/students_api/main.go
+++ project/students_api/cmd/students_api/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/tejas-2232/students_api/internal/config"
 )
@@ -33,6 +38,7 @@ func main() {
 	// creating this because, if we interuppt the cmd, it will show the interupption in cmd
 	// like - exit status 0xc000013a
 	done := make(chan os.Signal, 1) // 1 because its buffered channel
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	// gracefully shutdown
 	go func() {
@@ -42,7 +48,23 @@ func main() {
 		}
 	}()
 
-	<-done // listening done channel
+	<-done // listening done -channel
 	// unti there is no signal in go channel, the code will not go further. We will  be blocked. Main prog will run continously
+
+	slog.Info("Shutting down the server")
+
+	//saving in var ctx(context)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
+	//logging
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+		// slog.String is used to concatenate the error
+	}
+
+	slog.Info("Server shutdown successfully")
 
 }
