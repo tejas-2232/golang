c26a968603e2ef03b9a682c008e00cef65f581d7
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-18 23:30:07 -0400

    file update

--- project/students_api/cmd/students_api/main.go
+++ project/students_api/cmd/students_api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/tejas-2232/students_api/internal/config"
 )
@@ -28,9 +29,20 @@ func main() {
 
 	fmt.Printf("server started %s", cfg.HTTPServer.Addr)
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	// create channel
+	// creating this because, if we interuppt the cmd, it will show the interupption in cmd
+	// like - exit status 0xc000013a
+	done := make(chan os.Signal, 1) // 1 because its buffered channel
+
+	// gracefully shutdown
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	<-done // listening done channel
+	// unti there is no signal in go channel, the code will not go further. We will  be blocked. Main prog will run continously
 
 }
