9113d6b9c9c4f208754c300400bbc379c63bce4e
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-22 22:16:46 -0400

    added student.go handler

--- project/students_api/internal/http/handlers/student/student.go
+++ project/students_api/internal/http/handlers/student/student.go
@@ -1,10 +1,36 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/tejas-2232/students_api/internal/types"
+	"github.com/tejas-2232/students_api/internal/utils/response"
+)
 
 func New() http.HandlerFunc {
 	// return data
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students API")) //converting string to byte slice & passing to write func
+
+		var student types.Student //see this struct in types/types.go
+
+		//r in r.Body can implement Decoder interface
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) { // target error is io.EOF
+			response.WriteJson(w, http.StatusBadRequest, err.Error()) // code 400
+			return
+		}
+
+		// serialize
+		// send json data to struct
+		slog.Info("creating a student")
+
+		// w.Write([]byte("Welcome to students API")) //converting string to byte slice & passing to write func
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"}) // code 201- created
 	}
 }
