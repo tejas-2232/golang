d1917a7912fa2f5229dac1a5048c90e1625e928e
tejas-2232 <tejasbachhav98@gmail.com>
2025-04-23 11:22:09 -0400

    json tags for struct add

--- project/students_api/internal/utils/response/response.go
+++ project/students_api/internal/utils/response/response.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string `json:"status`
+	Error  string `json:"error` // "Error" will be displayed as "error"
+} // because we want to send a json like that if we get any error
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
 func WriteJson(w http.ResponseWriter, status int, data interface{}) error {
 	// we are gonna pass write response object
 	// we wrote any for datatype of data like= "data any", because we don't know what type of data we'll  be receiving
@@ -16,7 +26,16 @@ func WriteJson(w http.ResponseWriter, status int, data interface{}) error {
 
 	// for incoming data, to put in in struct we decoded it.
 	//we will need to encode here as we are converting struct info to json, so
-
 	return json.NewEncoder(w).Encode(data) // Encode method returns error -
+	// when we dont include json data in postman while testing it throws "EOF" response
+	// it should throw json -
+	// ######################################
+	// solution- add Response struct and create one generalError method
+}
 
+func GeneralError(err error) Response {
+	return Response{
+		Status: StatusError,
+		Error:  err.Error(),
+	}
 }
