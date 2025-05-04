d12721de9ce92f1d9d53fed49de867f4c06bd045
tejas-2232 <tejasbachhav98@gmail.com>
2025-05-04 18:11:00 -0400

    updateByID handle func add

--- project/students_api/cmd/students_api/main.go
+++ project/students_api/cmd/students_api/main.go
@@ -35,6 +35,8 @@ func main() {
 	router.HandleFunc("POST /api/students", student.New(storage))
 	router.HandleFunc("GET /api/students/{id}", student.GetByID(storage))
 	router.HandleFunc("GET /api/students", student.GetList(storage))
+	router.HandleFunc("PUT /api/students/{id}", student.UpdateByID(storage))
+	// router.HandleFunc("DELETE /api/students/{id}", student.DeleteByID(storage))
 
 	// setup server
 	server := http.Server{
@@ -44,7 +46,7 @@ func main() {
 
 	slog.Info("server started %s", slog.String("address", cfg.HTTPServer.Addr))
 
-	fmt.Printf("server started at", cfg.HTTPServer.Addr)
+	fmt.Printf("server started at %s", cfg.HTTPServer.Addr)
 
 	// create channel
 	// creating this because, if we interuppt the cmd, it will show the interupption in cmd
